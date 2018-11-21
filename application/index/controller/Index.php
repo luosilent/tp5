078e27e775b5380930e82ca750c7985efa6d0015
luosilent <luosilent@qq.com>
2018-11-21 17:31:50 +0800

    tp5

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,21 +1,49 @@
 <?php
+
 namespace app\index\controller;
 
 use think\Controller;
 use think\Db;
-use think\Request;
 
 class Index extends Controller
 {
     public function index()
     {
-        $data = Db::name('data')->select();
+        $data = Db::name('data')
+            ->where('id','>',1)
+            ->limit(10)
+            ->select();
         $this->assign('result', $data);
         return $this->fetch();
     }
-    public function hello(Request $request, $name = 'World')
+
+    public function index2($name = '')
     {
-        echo 'url: ' . $request->url() . '<br/>';
-        return 'Hello,' . $name . '！';
+        if ('thinkphp' == $name) {
+            $this->success('欢迎使用ThinkPHP5.0', 'he');
+        } else {
+            $this->error('错误的name', 'guest');
+        }
+    }
+
+    public function hello($name = 'World')
+    {
+        echo 'url: ' . $this->request->url(true) . '<br/>';
+        echo 'url: ' . $this->request->baseUrl() . '<br/>';
+        echo 'name1:' . $this->request->param('name') . '<br/>';
+        echo 'name2:' . $this->request->post('name') . '<br/>';
+        echo 'ip:' . $this->request->ip() . '<br/>';
+
+        return 'Hello,' . $name . '！' . '<br/>';
+    }
+
+    public function he()
+    {
+        return 'Hello,ThinkPHP!';
+    }
+
+    public function guest()
+    {
+        return 'Hello,Guest!';
     }
 }
